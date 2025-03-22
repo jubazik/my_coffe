fd2eddca21069d1965cbc2d35722c44e47a02795
jubazik <hitgas12@mail.ru>
2025-03-22 13:15:15 +0300

    добавили возможность редактировать в главной старнице поле статус заказа и на старнице самого заказа

--- corn/corn/settings.py
+++ corn/corn/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'directory.apps.DirectoryConfig',
-    'countries.apps.CountriesConfig',
-
+    'counterparties.apps.CounterpartiesConfig',
+    
 ]
 
 MIDDLEWARE = [
--- corn/corn/urls.py
+++ corn/corn/urls.py
@@ -22,7 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('directory.urls')),
-    path('warehouse/', include('warehouse.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
