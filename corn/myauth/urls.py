4449cce5541d6870d65471854956a076e4cf15a8
jubazik <hitgas12@mail.ru>
2025-04-12 21:50:04 +0300

    доработки

--- corn/myauth/urls.py
+++ corn/myauth/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import (
     set_cookie_view,
     get_cookie_view,
-    # login_view,
+    login_view,
     logout_view,
     AboutMeView,
     RegisterView,
@@ -20,7 +20,7 @@ urlpatterns = [
          name='login'),
 
     path('logout/', logout_view, name='logout'),
-    # path('login/', login_view, name='login'),
+    path('login/', login_view, name='login'),
     path('about-me/', AboutMeView.as_view(), name='about-me'),
     path('register/', RegisterView.as_view(), name='register'),
     path('cookie/get/', get_cookie_view, name='cookie_get'),
