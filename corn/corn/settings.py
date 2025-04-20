331de4c3d11f6d46b3197bc362d1f2607dd7a496
jubazik <hitgas12@mail.ru>
2025-04-20 21:45:41 +0300

    подключил postgresql

--- corn/corn/settings.py
+++ corn/corn/settings.py
@@ -13,7 +13,8 @@ import os
 from pathlib import Path
 
 from django.urls import reverse_lazy
-
+import psycopg2
+print(psycopg2.__file__)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,13 +79,22 @@ WSGI_APPLICATION = 'corn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': db.sqlite3 / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'base',
+        'USER': 'youruser',  # Ваше системное имя или созданный пользователь
+        'PASSWORD': 'shuher123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
