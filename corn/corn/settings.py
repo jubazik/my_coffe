2a74616081bce600ed6820ac8310c00ce86bdcd6
jubazik <hitgas12@mail.ru>
2025-04-24 19:07:10 +0300

    тестовый запуск

--- corn/corn/settings.py
+++ corn/corn/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)j5f+^!yq9hk1fu%d0_9!33*jf4c836=fh3c=rff94idmd&-a2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.corn.ru','193.32.176.17']
 
 # Application definition
 
