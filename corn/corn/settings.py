4b459601341fec5437a52ec9cee4f9100048a0d9
jubazik <hitgas12@mail.ru>
2025-04-26 19:59:12 +0300

    отключил psql

--- corn/corn/settings.py
+++ corn/corn/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+from django.contrib.sessions.backends import db
 from django.urls import reverse_lazy
 import psycopg2
 print(psycopg2.__file__)
@@ -80,22 +81,22 @@ WSGI_APPLICATION = 'corn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': db.sqlite3 / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'base',
-        'USER': 'juba',  # Ваше системное имя или созданный пользователь
-        'PASSWORD': 'shuher123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'base',
+#         'USER': 'juba',  # Ваше системное имя или созданный пользователь
+#         'PASSWORD': 'shuher123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
